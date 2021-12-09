2b5d3c4c640b2ad119b8fa69b3385183ec85ce66
karim haidar <karimhai@umich.edu>
2021-12-09 16:54:09 -0500

    added pos and negative tests

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -11,7 +11,7 @@ testDuplicates();
 }
     public testSelectionSort() {
     }
-    
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -19,29 +19,49 @@ testDuplicates();
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
-        /** add tests to check for this unit test **/
+
+        SelectionSort temp= new SelectionSort();
+          temp.basicSelectionSort(arr);
+
+          assertArrayEquals("test positive failed",Sortedarr,arr);
     }
-    
+
     public void testNegative(){
-        
-        /** Test data contains negative values only **/
+      int[] arr = new int[5];
+     arr[0] = -8;
+     arr[1] = -9;
+     arr[2] = -7;
+     arr[3] = -10;
+     arr[4] = -2;
+
+     int[] Sortedarr = new int[5];
+     Sortedarr[0] = -10;
+     Sortedarr[1] = -9;
+     Sortedarr[2] = -8;
+     Sortedarr[3] = -7;
+     Sortedarr[4] = -2;
+
+     SelectionSort temp= new SelectionSort();
+     temp.basicSelectionSort(arr);
+     assertArrayEquals("test negative failed",Sortedarr,arr);
+     /** Test data contains negative values only **/
+ 
     }
-    
+
     public void testMixed(){
-        
+
         /** Test data contains with both positive, negative and zeros **/
     }
-    
+
     public void testDuplicates(){
-        
+
         /** Test data contains duplicates **/
     }
 }
