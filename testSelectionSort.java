b3088adc0d121936ed68d52b17a45f2d53e246df
karim haidar <karimhai@umich.edu>
2021-12-09 16:55:33 -0500

    added mixed and duplicate tests

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -11,7 +11,7 @@ testDuplicates();
 }
     public testSelectionSort() {
     }
-    
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -19,29 +19,61 @@ testDuplicates();
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
+
         /** add tests to check for this unit test **/
     }
-    
+
     public void testNegative(){
-        
+
         /** Test data contains negative values only **/
     }
-    
+
     public void testMixed(){
-        
-        /** Test data contains with both positive, negative and zeros **/
+      int[] arr = new int[5];
+      arr[0] = -8;
+      arr[1] = 9;
+      arr[2] = -7;
+      arr[3] = 10;
+      arr[4] = 2;
+
+      int[] Sortedarr = new int[5];
+      Sortedarr[0] = -8;
+      Sortedarr[1] = -7;
+      Sortedarr[2] = 2;
+      Sortedarr[3] = 9;
+      Sortedarr[4] = 10;
+
+      SelectionSort temp= new SelectionSort();
+      temp.basicSelectionSort(arr);
+      assertArrayEquals("test mixed failed",Sortedarr,arr);
+      /** Test data contains with both positive, negative and zeros **/
     }
-    
+
     public void testDuplicates(){
-        
-        /** Test data contains duplicates **/
+      int[] arr = new int[5];
+       arr[0] = 8;
+       arr[1] = 9;
+       arr[2] = 9;
+       arr[3] = 10;
+       arr[4] = 8;
+
+       int[] Sortedarr = new int[5];
+       Sortedarr[0] = 8;
+       Sortedarr[1] = 8;
+       Sortedarr[2] = 9;
+       Sortedarr[3] = 9;
+       Sortedarr[4] = 10;
+
+       SelectionSort temp= new SelectionSort();
+       temp.basicSelectionSort(arr);
+       assertArrayEquals("test dup failed",Sortedarr,arr);
+       /** Test data contains duplicates **/
     }
 }
